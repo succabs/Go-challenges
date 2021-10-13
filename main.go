e44b833ca932cb7608e816137521504084a0e70d
Arttu <artnikkila@gmail.com>
2021-10-13 19:31:41 +0300

    Updated answer to first solution, not correct yet

--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import "fmt"
 
-func multiple(a int) []int {
+//function to solve the first project euler problem
+func multiple(a int, b int) int {
 	list := []int{}
 	for i := 0; i < 1000; i++ {
 		if i%a == 0 {
 			list = append(list, i)
 		}
+		if i%b == 0 {
+			list = append(list, i)
+		}
+	}
+	removeDuplicateInt(list)
+	sum := 0
+	for _, num := range list {
+		sum += num
+	}
+	return sum
+}
+
+//function from https://stackoverflow.com/questions/66643946/
+//how-to-remove-duplicates-strings-or-int-from-slice-in-go
+//to remove duplicate ints on slice
+func removeDuplicateInt(intSlice []int) []int {
+	allKeys := make(map[int]bool)
+	list := []int{}
+	for _, item := range intSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
 	}
 	return list
 }
 
+//main to call and print the functions above
 func main() {
-	fmt.Println(multiple(3))
+	fmt.Println("The first Project Euler problem answer:")
+	fmt.Println(multiple(3, 5))
 }
